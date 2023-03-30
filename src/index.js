4592968bd773cdfc642011ff02a98928d33b3900
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-03-30 21:49:58 +0300

    add

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // import refs from './js/refs';
 
+const KEY = '34781743-09d11a08c8aa729d147b2c9f6';
+const BASE_URL = 'https://pixabay.com/api/';
+
 const searchFormEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
 const loadMoreBtnEl = document.querySelector('.load-more');
@@ -20,25 +23,33 @@ searchFormEl.addEventListener('submit', onSearch);
 
 async function onSearch(event) {
   event.preventDefault();
+  resetPage();
   searchValue = event.currentTarget.searchQuery.value.trim();
+  console.log(searchValue);
   currentPage = 1;
   if (searchValue === '') {
     // galleryEl.innerHTML = '';
     console.log('empty');
     return;
   }
+  const result = await addImagesList();
+  console.log(result);
+
+  const data = await addImagesList(page, searchValue);
 }
-const KEY = '34781743-09d11a08c8aa729d147b2c9f6';
-const BASE_URL = 'https://pixabay.com/api/';
 
 addImagesList();
-function addImagesList() {
+async function addImagesList() {
   return fetch(
-    `${BASE_URL}?key=${KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=4&page=${currentPage}`
+    `${BASE_URL}?key=${KEY}&q=${searchValue}&type=photo&orientation=horizontal&safesearch=true&per_page=10&page=${currentPage}`
   ).then(response => {
     if (response.ok) {
       return response.json();
     }
     throw new Error(response.statusText);
   });
+}
+
+function resetPage() {
+  currentPage = 1;
 }
