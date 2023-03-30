d8e8f9887ac5969fa7f315307802d838ad6c8edc
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-03-30 20:40:00 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,36 +1,44 @@
 import './sass/_common.scss';
 import axios from 'axios';
 import Notiflix from 'notiflix';
-import NewsApiService from './js/news-api-service';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// import refs from './js/refs';
 
-const refs = {
-  searchForm: document.querySelector('#search-form'),
-  gallery: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
-};
-const newsApiService = new NewsApiService();
+const searchFormEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtnEl = document.querySelector('.load-more');
 
-let perPage = 40;
+let searchValue = '';
+let gallery = new SimpleLightbox('.gallery a');
+let perPage = 10;
 let currentPage = 1;
 let currentHits = 0;
 
-refs.searchForm.addEventListener('submit', onSearch);
-// refs.loadMoreBtn.addEventListener('click', onLoadMore);
+searchFormEl.addEventListener('submit', onSearch);
+// refs.loadMoreBtnEl.addEventListener('click', onLoadMore);
 
-function onSearch(e) {
-  e.preventDefault();
-  newsApiService.query = e.currentTarget.searchQuery.value.trim();
-  newsApiService.fetchArticles();
+async function onSearch(event) {
+  event.preventDefault();
+  searchValue = event.currentTarget.searchQuery.value.trim();
+  currentPage = 1;
+  if (searchValue === '') {
+    // galleryEl.innerHTML = '';
+    console.log('empty');
+    return;
+  }
+}
+const KEY = '34781743-09d11a08c8aa729d147b2c9f6';
+const BASE_URL = 'https://pixabay.com/api/';
 
-  // if (searchQuery === '') {
-  //   return;
-  // }
+addImagesList();
+function addImagesList() {
+  return fetch(
+    `${BASE_URL}?key=${KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=4&page=${currentPage}`
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  });
 }
-function onLoadMore(params) {
-  newsApiService.fetchArticles();
-}
-// function noMatchingSearch() {
-//   Notiflix.Notify.failure(
-//     'Sorry, there are no images matching your search query. Please try again.'
-//   );
-// }
