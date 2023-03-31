10e9bd859b56fe34b26d8d069875d5c6618b16f5
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-03-31 20:21:18 +0300

    add

--- src/index.js
+++ src/index.js
@@ -12,63 +12,84 @@ const searchFormEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
 const loadMoreBtnEl = document.querySelector('.load-more');
 
-let searchValue = '';
-let gallery = new SimpleLightbox('.gallery a');
-let perPage = 40;
+let lastSearchValue = '';
+// let gallery = new SimpleLightbox('.gallery a');
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+let perPage = 3;
 let currentPage = 1;
-let currentHits = 0;
-let hits = 0;
+
 // ------робимо кноку loadMoreBtn невидимою-------
 loadMoreBtnEl.style.display = 'none';
 
 searchFormEl.addEventListener('submit', onSearch);
-// refs.loadMoreBtnEl.addEventListener('click', onLoadMore);
+loadMoreBtnEl.addEventListener('click', onLoadMore);
 
 async function onSearch(event) {
   event.preventDefault();
   searchValue = event.currentTarget.searchQuery.value.trim();
-  console.log(searchValue);
-  currentPage = 1;
   //-------якщо вікно пошуку пусте, то повідомлення
   if (searchValue === '') {
-    galleryEl.refresh();
     Notiflix.Notify.warning('Please enter a keyword to continue the search');
-    console.log('empty');
     return;
   }
+  if (searchValue === lastSearchValue) {
+    currentPage += 1;
+  }
+  if (searchValue !== lastSearchValue) {
+    lastSearchValue = searchValue;
+    currentPage = 1;
+    galleryEl.innerHTML = '';
+  }
+
   const data = await addImagesList(currentPage, searchValue);
-  console.log(data);
+  // console.log(data);
   const arrayOfResults = data.hits;
-  // console.log(arrayOfResults);
-  // console.log(data.totalHits);
-  //-------якщо результат пошуку 0, то повідомлення
+
+  //-------якщо результат пошуку 0, то повідомлення------//
   if (data.totalHits === 0) {
     page = 1;
     Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
-    galleryEl.refresh();
+
     return;
   }
-  // ----якщо результат пошуку - непорожній масив, то
+  // --якщо результат пошуку - непорожній масив, то повідомлення---//
   if (data.hits.length > 0) {
     markupCard(arrayOfResults);
     Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
     loadMoreBtnEl.style.display = 'block';
-    galleryEl.refresh();
   }
 }
-
-addImagesList();
-async function addImagesList() {
-  return fetch(
-    `${BASE_URL}?key=${KEY}&q=${searchValue}&type=photo&orientation=horizontal&safesearch=true&per_page=10&page=${currentPage}`
-  ).then(response => {
-    if (response.ok) {
-      return response.json();
+async function onLoadMore() {
+  let data = await addImagesList(lastSearchValue, currentPage);
+  const arrayOfResults = data.hits;
+  // console.log(arrayOfResults);
+  // console.log(data.totalHits);
+  if (arrayOfResults.length > 0) {
+    markupCard(arrayOfResults);
+    loadMoreBtnEl.style.display = 'block';
+    if (currentPage === 1) {
+      Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
     }
-    throw new Error(response.statusText);
-  });
+    currentPage += 1;
+  }
+   if (currentPage > 1 && secondRequestOutofData === false) {
+      endOfPicturesNotification();
+      console.log('Збіги закінчилися');
+      secondRequestOutofData = true;
+    }
+  }
+}
+async function addImagesList() {
+  const response = await fetch(
+    `${BASE_URL}?key=${KEY}&q=${searchValue}&type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${currentPage}`
+  );
+
+  const promice = response.json();
+  return promice;
 }
 
 function markupCard(arrayOfResults) {
@@ -96,6 +117,7 @@ function markupCard(arrayOfResults) {
     .join('');
 
   galleryEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
 
 function resetPage() {
